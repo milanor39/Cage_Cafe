e675d185f5303518b478b71e2a78464e73e6a972
洪銘鴻 <hjwing237@hotmail.com>
2022-08-24 17:51:16 +0800

    上傳檔案

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <header>
-    <div class="container">
-      <div class="header-content">
+    <div class="header-content">
         <div class="logo">
           <router-link to="/">
             <img src="./assets/logo.png" alt="logo">
@@ -11,21 +10,23 @@
         <nav>
           <ul class="nav-link">
             <li>
-              <router-link to="/about" name="about">關於我們</router-link>
+              <router-link to="/">首頁</router-link>
             </li>
             <li>
               <router-link to="/about">最新消息</router-link>
             </li>
             <li>
+              <router-link to="/about">關於我們</router-link>
+            </li>
+            <li>
               <router-link to="/about">美味餐點</router-link>
             </li>
             <li>
-              <router-link to="/about">分店情報</router-link>
+              <router-link to="/about">門市資訊</router-link>
             </li>
           </ul>
         </nav>
       </div>
-    </div>
   </header>
   <main>
     <div class="container">
@@ -33,11 +34,43 @@
     </div>
   </main>
   <footer>
-    <div class="container">footer</div>
+    <div class="container">
+      <div class="footer-content">
+        <div class="sns">
+          <ul>
+            <li><a href="#" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"></i></a></li>
+            <li><a href="#" target="_blank"><i class="fa-brands fa-instagram fa-2x"></i></a></li>
+            <li><a href="#" target="_blank"><i class="fa-brands fa-twitter fa-2x"></i></a></li>
+          </ul>
+        </div>
+        <div class="footer-list">
+          <ul>
+            <li>
+              <router-link to="/about">企業社會責任</router-link>
+            </li>
+            <li>
+              <router-link to="/about">網站地圖</router-link>
+            </li>
+            <li>
+              <router-link to="/about">徵才情報</router-link>
+            </li>
+            <li>
+              <router-link to="/about">聯絡我們</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <p class="copy"><small>&copy; 2022 CageCafe</small></p>
+    </div>
   </footer>
 </template>
 
 <style lang="scss">
+:root{
+  --main-brown: #734338;
+  --main-white: #FCFAF2;
+  --main-black: #554236;
+}
 body{
   margin: 0;
 }
@@ -47,12 +80,17 @@ body{
 }
 /*header*/
 header{
-  border-bottom: 10px solid brown;
+  border-bottom: 10px solid var(--main-brown);
+  padding: 0 20px;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  padding-top: 10px;
   .header-content{
     display: flex;
     justify-content: space-between;
     .logo{
-      width: 300px;
+      width: 210px;
       white-space: nowrap;
       overflow: hidden;
       h1{
@@ -62,7 +100,7 @@ header{
   }
   nav{
     display: flex;
-    align-items: flex-end;
+    align-items: center;
     .nav-link{
       display: flex;
       list-style: none;
@@ -70,10 +108,64 @@ header{
       margin: 0;
       a{
         text-decoration: none;
-        font-size: 1.5rem;
+        font-size: 1.2rem;
+        font-weight: bold;
+        padding: 10px 25px;
+        color: var(--main-black);
+      }
+      a:hover{
+        background-color: var(--main-brown);
+        color: var(--main-white);
       }
     }
   }
 }
-
+footer{
+  background-color: var(--main-brown);
+  padding: 10px;
+  .footer-content{
+    display: flex;
+    justify-content: space-between;
+    .sns{
+        display: flex;
+        align-items: center;
+        ul{
+          display: flex;
+          justify-content: space-between;
+          list-style: none;
+          padding-left: 0;
+          a{
+            color: var(--main-white);
+            text-decoration: none;
+            margin-right: 20px;
+          }
+        }
+      }
+    .footer-list{
+      display: flex;
+      align-items: center;
+      ul{
+        display: flex;
+        justify-content: space-between;
+        list-style: none;
+        padding-left: 0;
+        a{
+          color: var(--main-white);
+          text-decoration: none;
+          margin-right: 20px;
+          font-size: 1rem;
+          border-bottom: 1px solid var(--main-brown);
+        }
+        a:hover{
+          border-bottom: 1px solid var(--main-white);
+        }
+      }
+    }
+  }
+  .copy{
+    margin-bottom: 0;
+    text-align: center;
+    color: var(--main-white);
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
-
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <img src="https://pakutaso.cdn.rabify.me/shared/img/thumb/NKJ56_coffeecok.jpg.webp?d=1420" alt="">
+  <img src="https://pakutaso.cdn.rabify.me/shared/img/thumb/NKJ56_coffeecok.jpg.webp?d=1420" alt="">
+  <img src="https://pakutaso.cdn.rabify.me/shared/img/thumb/NKJ56_coffeecok.jpg.webp?d=1420" alt="">
 </template>
 
 <script>
